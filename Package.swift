3649449133081f63c5a4d93fdbf448855d3a2e55
Jonathan Ivan Ramirez Partida <jirp.djnavi@gmail.com>
2026-07-29 13:19:09 -0600

    Initialize Swift package for MTSlideToOpen

---
+++ Package.swift
@@ -0,0 +1,22 @@
+// swift-tools-version:5.3
+import PackageDescription
+
+let package = Package(
+    name: "MTSlideToOpen",
+    platforms: [
+        .iOS(.v11) // Puedes ajustar la versión de iOS según necesites
+    ],
+    products: [
+        .library(
+            name: "MTSlideToOpen",
+            targets: ["MTSlideToOpen"]),
+    ],
+    dependencies: [],
+    targets: [
+        .target(
+            name: "MTSlideToOpen",
+            dependencies: [],
+            path: "Sources/MTSlideToOpen"
+        )
+    ]
+)
